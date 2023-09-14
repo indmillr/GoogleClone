d65af4c28b1cc3cd94a6d9f27c7b6fda1d6d7537
indmillr <indmillr@gmail.com>
2023-09-14 15:45:12 -0500

    FEAT: Fetch Data from Google Search API

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ImageSearchPage() {
   return <div>ImageSearchPage</div>;
 }
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+  const data = await response.json();
+  const results = data.items;
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
