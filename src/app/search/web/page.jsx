3e4d34339fc05ed2e60fed6a287c0503abecc8e5
indmillr <indmillr@gmail.com>
2023-09-15 14:09:29 -0500

    Component: Pagination

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -5,10 +5,11 @@ import Link from "next/link";
 import WebSearchResults from "@/components/WebSearchResults";
 
 export default async function WebSearchPage({ searchParams }) {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const startIndex = searchParams.start || "1";
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
 
   if (!response.ok) {
--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,9 +1,12 @@
 // NEXT TOOLS
 import Link from "next/link";
 
+// COMPONENTS
+import PaginationButtons from "./PaginationButtons";
+
 export default function ImageSearchResults({ results }) {
   return (
-    <div className="pb-24 mt-4">
+    <div className="sm:pb-24 pb-40 mt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items.map((result) => (
           <div key={result.link} className="mb-8">
@@ -12,7 +15,7 @@ export default function ImageSearchResults({ results }) {
                 <img
                   src={result.link}
                   alt={result.title}
-                  className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow"
+                  className="group-hover:shadow-xl h-60 w-full object-contain transition-shadow"
                 />
               </Link>
               <Link href={result.image.contextLink}>
@@ -28,6 +31,9 @@ export default function ImageSearchResults({ results }) {
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+        <PaginationButtons />
       </div>
     </div>
   );
